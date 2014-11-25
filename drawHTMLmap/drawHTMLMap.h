965d4c7fb78f26b0a3d5659cdc1762c03a5cb339
KombuchaShroomz <1102607d@student.gla.ac.uk>
2014-11-25 21:18:58 +0000

    Restructured folders

---
+++ drawHTMLmap/drawHTMLMap.h
@@ -0,0 +1,14 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <cfloat>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+using std::vector;
+using std::string;
+
+// Class used to draw a html representation of a map
+
+int drawMap(vector< vector<float > >, string);
