624f49061fe4c6759f2503b2f390b9def95e28ac
Gavin Davidson <gavaran@hotmail.com>
2015-02-25 16:57:32 +0000

    Added big map test script

---
+++ parallel_SOM/big_map_test_script.sh
@@ -0,0 +1,32 @@
+echo "== TEST SCRIPT FOR PARALLEL SOM: BIG MAPS =="
+echo -e "\t== Test 1 =="
+NOW=`date`
+echo -e "\tStart\t" $NOW
+echo -e "\tRunning 32 x 32 map \t20480 x 3 vectors"
+./parallel_SOM datasets/input_20480_3_gauss.dat 32 > datasets/test_1_big_output.txt
+NOW=`date`
+echo -e "\tEnd\t" $NOW
+
+echo -e "\t== Test 2 =="
+NOW=`date`
+echo -e "\tStart\t" $NOW
+echo -e "\tRunning 64 x 64 map \t20480 x 3 vectors"
+./parallel_SOM datasets/input_20480_3_gauss.dat 64 > datasets/test_2_big_output.txt
+NOW=`date`
+echo -e "\tEnd\t" $NOW
+
+echo -e "\t== Test 3 =="
+NOW=`date`
+echo -e "\tStart\t" $NOW
+echo -e "\tRunning 96 x 96 map \t20480 x 3 vectors"
+./parallel_SOM datasets/input_20480_3_gauss.dat 96 > datasets/test_3_big_output.txt
+NOW=`date`
+echo -e "\tEnd\t" $NOW
+
+echo -e "\t== Test 4 =="
+NOW=`date`
+echo -e "\tStart\t" $NOW
+echo -e "\tRunning 128 x 128 map \t20480 x 3 vectors"
+./parallel_SOM datasets/input_20480_3_gauss.dat 128 > datasets/test_4_big_output.txt
+NOW=`date`
+echo -e "\tEnd\t" $NOW
